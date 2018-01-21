c47b26f95b80e8498c1416a3d113310095e83c00
YangJongYeol <yajya004@gmail.com>
2018-01-22 01:38:17 +0900

    Add 50A-Dimino piling

---
+++ 50A-Domino piling/50A-Domino piling/main.cpp
@@ -0,0 +1,20 @@
+//
+//  main.cpp
+//  50A-Domino piling
+//
+//  Created by 양종열 on 2018. 1. 22..
+//  Copyright © 2018년 양종열. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    int m, n;
+    cin>>m>>n;
+    
+    cout<<(m*n)/2<<endl;
+    
+    return 0;
+}
