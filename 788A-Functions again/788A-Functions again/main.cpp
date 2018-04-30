3ad69d8af5c6646f6352017ff6b6a687e064b77a
YangJongYeol <yajya004@gmail.com>
2018-04-30 19:14:43 +0900

    Add 788A solution

--- 788A-Functions again/788A-Functions again/main.cpp
+++ 788A-Functions again/788A-Functions again/main.cpp
@@ -7,9 +7,51 @@
 //
 
 #include <iostream>
+#include <vector>
+#include <cmath>
+using namespace std;
+int n;
+vector<int> vc;
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    ios::sync_with_stdio(false);
+    
+    cin>>n;
+    
+    int tmp;
+    for (int i=0; i<n; i++) {
+        cin>>tmp;
+        vc.push_back(tmp);
+    }
+    
+    long long dp[n];
+    dp[0] = 0;
+    for (int i=1; i<n; i++) {
+        dp[i] = abs(vc[i-1] - vc[i]);
+    }
+    
+    long long dp_2[n];
+    dp_2[0] = dp[0];
+    for (int i=1; i<n; i++) {
+        if (i % 2 == 1) {
+            dp_2[i] = dp_2[i-1] + dp[i];
+        } else {
+            dp_2[i] = dp_2[i-1] - dp[i];
+        }
+    }
+    
+    long long min = 2000000000;
+    long long max = 0;
+    for (int i=0; i<n; i++) {
+        if (min > dp_2[i]) {
+            min = dp_2[i];
+        }
+        if (max < dp_2[i]) {
+            max = dp_2[i];
+        }
+    }
+    
+    cout<<abs(max-min);
     return 0;
 }
